be7517cadfcfa2537a14c4ae8145039f040d5401
kymfeltus-oss <kymfeltus@gmail.com>
2025-11-02 22:56:14 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
-function showMessage(message) {
-  alert(message);
-}
+// Adds a subtle click event on the logo
+document.querySelector(".logo").addEventListener("click", () => {
+  alert("Welcome to the John Heath Influence App!");
+});
