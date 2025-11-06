1c75b3bf7dae12bdf9d7b947bb3de4a59d7f5008
kymfeltus-oss <kymfeltus@gmail.com>
2025-11-05 22:54:32 -0600

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-// Smooth logo pulse and welcome message
-const logo = document.querySelector(".logo");
-
-if (logo) {
-  logo.addEventListener("click", () => {
-    alert("Welcome to the John Heath Influence App!");
-  });
-}
